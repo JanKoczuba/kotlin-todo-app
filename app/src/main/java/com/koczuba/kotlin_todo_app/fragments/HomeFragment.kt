bcf335bdbbbd59548dae70d8fdcc74dfcab8021b
jankoczuba <jan.koczuba@it-solve.pl>
2024-05-05 16:37:00 +0200

    added open add task dialog function to home screen

--- app/src/main/java/com/koczuba/kotlin_todo_app/fragments/HomeFragment.kt
+++ app/src/main/java/com/koczuba/kotlin_todo_app/fragments/HomeFragment.kt
@@ -5,56 +5,60 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.koczuba.kotlin_todo_app.R
+import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.koczuba.kotlin_todo_app.databinding.FragmentHomeBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class HomeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+class HomeFragment : Fragment(), AddToDoDialogFragment.OnDialogAddTaskButtonClickListener {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: FragmentHomeBinding
+    private lateinit var database: DatabaseReference
+    private lateinit var auth: FirebaseAuth
+    private var addTaskDialogFragment: AddToDoDialogFragment? = null
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        init(view)
+        binding.addTaskButton.setOnClickListener {
+            registerEvents()
+        }
+
+
     }
+
+    private fun init(view: View) {
+        auth = FirebaseAuth.getInstance()
+        database = FirebaseDatabase.getInstance().reference
+
+    }
+
+    private fun registerEvents() {
+        addTaskDialogFragment = AddToDoDialogFragment()
+        addTaskDialogFragment!!.setListener(this)
+        addTaskDialogFragment!!.show(
+            childFragmentManager,
+            "AddToDoDialogFragment"
+        )
+    }
+
+    override fun saveTask(todoTask: String, todoEdit: TextInputEditText) {
+        TODO("Not yet implemented")
+    }
+
 }
